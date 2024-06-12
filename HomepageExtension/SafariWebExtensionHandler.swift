21210f2db57acf0498501e2b1ca5eaf9e23fec99
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2024-06-12 19:00:15 +0100

    Refactor extension handler

--- HomepageExtension/SafariWebExtensionHandler.swift
+++ HomepageExtension/SafariWebExtensionHandler.swift
@@ -12,6 +12,7 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
         let request = context.inputItems.first as? NSExtensionItem
+        let defaultPage = "options.html"
 
         let profile: UUID?
         if #available(iOS 17.0, macOS 14.0, *) {
@@ -23,28 +24,30 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
         let message: String
         if #available(iOS 17.0, macOS 14.0, *) {
             let info = request?.userInfo?[SFExtensionMessageKey] as? AnyObject
-            message = info?["message"] as? String ?? "options.html"
+            message = info?["message"] as? String ?? defaultPage
         } else {
             let info = request?.userInfo?["message"] as? AnyObject
-            message = info?["message"] as? String ?? "options.html"
+            message = info?["message"] as? String ?? defaultPage
         }
+
+        let describedMessage = String(describing: message)
 
         #if DEBUG
         os_log(
             .default,
             "Received message from browser.runtime.sendNativeMessage: %@ (profile: %@)",
-            String(describing: message),
+            describedMessage,
             profile?.uuidString ?? "none"
         )
         #endif
 
         // Migrate homepage from older versions
-        if UserDefaults.homepage == nil && String(describing: message) != "options.html" {
-            UserDefaults.homepage = String(describing: message)
+        if UserDefaults.homepage == nil && describedMessage != defaultPage {
+            UserDefaults.homepage = describedMessage
         }
 
         let response = NSExtensionItem()
-        let url: String = UserDefaults.homepage ?? "options.html"
+        let url: String = UserDefaults.homepage ?? defaultPage
         response.userInfo = [ SFExtensionMessageKey: [ "url": url ] ]
 
         context.completeRequest(returningItems: [ response ], completionHandler: nil)

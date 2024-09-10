d6a3eb9db805e2f4adbfce3d7f433d45eaca6546
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2024-09-10 19:44:05 +0100

    Add iOS control to open app

---
+++ HomepageWidget/HomepageWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HomepageWidgetBundle.swift
+//  HomepageWidget
+//
+//  Created by Ahnaf Mahmud on 10/09/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomepageWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SetHomepageButton()
+    }
+}
---
+++ HomepageWidget/SetHomepageButton.swift
@@ -0,0 +1,23 @@
+//
+//  SetHomepageButton.swift
+//  Homepage
+//
+//  Created by Ahnaf Mahmud on 10/09/2024.
+//
+
+import SwiftUI
+import WidgetKit
+
+/// A button to open the Homepage app
+struct SetHomepageButton: ControlWidget {
+    var body: some ControlWidgetConfiguration {
+        StaticControlConfiguration(
+            kind: "com.ip18.Homepage.SetHomepageButton"
+        ) {
+            ControlWidgetButton(action: OpenAppIntent()) {
+                Label("Set Homepage", image: "HouseGear")
+            }
+        }
+        .displayName("Set Homepage")
+    }
+}

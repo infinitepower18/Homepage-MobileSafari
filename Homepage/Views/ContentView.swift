02b1ae6d7d536eeb4050f4f9d8a3923601442567
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2025-03-20 22:13:29 +0000

    Update copyright

--- Homepage/Views/ContentView.swift
+++ Homepage/Views/ContentView.swift
@@ -132,7 +132,7 @@ struct ContentView: View {
     private var aboutAlert: Alert {
         Alert(
             title: Text("appName"),
-            message: Text("version \(UIApplication.appVersion ?? "unknown")") + Text(verbatim: "\n© 2024 Ahnaf Mahmud"),
+            message: Text("version \(UIApplication.appVersion ?? "unknown")") + Text(verbatim: "\n© 2025 Ahnaf Mahmud"),
             primaryButton: .cancel(Text("close")),
             secondaryButton: .default(Text("support")) {
                 if let supportUrl {

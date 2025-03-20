3ab244bcdf99502e80fbb2cc8884818e884e36d1
Ahnaf Mahmud <44692189+infinitepower18@users.noreply.github.com>
2025-03-20 22:27:48 +0000

    Remove migration code

--- HomepageExtension/Resources/background.js
+++ HomepageExtension/Resources/background.js
@@ -1,25 +1,17 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.storage.sync.get({
-      homepage: "options.html",
-    }, function(items) {
-      chrome.runtime.sendNativeMessage("application.id", { message: items.homepage }, function(response) {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-          return;
-        }
+  chrome.runtime.sendNativeMessage("application.id", { message: "get-homepage" }, function(response) {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
 
-        // Check if response contains the expected "url" key
-        if (response && response.url) {
-          // Clear once migrated to UserDefaults
-          if (items.homepage != "options.html") {
-            chrome.storage.sync.clear();
-          }
-            chrome.tabs.update({
-              url: response.url
-            });
-        } else {
-          console.error("No URL received in the response.");
-        }
+    // Check if response contains the expected "url" key
+    if (response && response.url) {
+      chrome.tabs.update({
+        url: response.url
       });
-    });
+    } else {
+      console.error("No URL received in the response.");
+    }
+  });
 });
